4228849bf66d733df68c131df1b211dfafe4a340
Coding <Coding@DESKTOP-GCA62MF>
2017-02-24 22:50:40 +0100

    new commit2

---
+++ src/java/al/edu/unyt/eliomartin/library/BookBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package al.edu.unyt.eliomartin.library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
+
+/**
+ *
+ * @author Coding
+ */
+@ManagedBean(name="bookBean")
+public class BookBean {
+    
+    @Resource(name="jdbc/addressbook")
+    DataSource dataSource;
+    
+    public ResultSet getBooks() throws SQLException {
+        String sql = "select * from books";
+        CachedRowSet rs= null;
+        try (Connection conn = dataSource.getConnection();
+                //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","CpXmax9w");
+                PreparedStatement st = conn.prepareStatement(sql)) {
+            rs = new com.sun.rowset.CachedRowSetImpl();
+            rs.populate(st.executeQuery());
+            return rs;
+        } 
+    }
+}
